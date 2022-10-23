52c50bda7067709c806d9b1a0caa19d0e70b3c36
RunyShark <spartadragon117@live.com.mx>
2022-10-23 13:07:58 -0500

    login

--- src/screens/LoadingScreen/index.tsx
+++ src/screens/LoadingScreen/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
+import {TextInput, TouchableOpacity} from 'react-native-gesture-handler';
 
 import {Background, WhiteLogo, loginStyles} from '../../';
 
@@ -7,8 +8,44 @@ export const LoadingScreen = () => {
   return (
     <>
       <Background />
-      <WhiteLogo />
-      <Text style={loginStyles.title}>Login</Text>
+      <View style={loginStyles.formContainer}>
+        <WhiteLogo />
+        <Text style={loginStyles.title}>Login</Text>
+        <Text style={loginStyles.label}>Email:</Text>
+        <TextInput
+          placeholder="email"
+          placeholderTextColor={'rgba(255,255,255,0.4)'}
+          keyboardType="email-address"
+          underlineColorAndroid={'white'}
+          style={loginStyles.inputField}
+          selectionColor="white"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={loginStyles.label}>Password:</Text>
+        <TextInput
+          placeholder="****"
+          placeholderTextColor={'rgba(255,255,255,0.4)'}
+          underlineColorAndroid={'white'}
+          style={loginStyles.inputField}
+          selectionColor="white"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <View style={loginStyles.buttonContainer}>
+          <TouchableOpacity activeOpacity={0.8} style={loginStyles.button}>
+            <Text style={loginStyles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={loginStyles.newUserContainer}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => console.log('w')}>
+            <Text style={loginStyles.buttonText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </>
   );
 };
