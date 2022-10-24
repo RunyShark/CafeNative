10f3a662c1b3e74b4e1007eec846bb505d543d11
RunyShark <spartadragon117@live.com.mx>
2022-10-23 19:22:48 -0500

    navigation

--- src/screens/ProductsScreen/index.tsx
+++ src/screens/ProductsScreen/index.tsx
@@ -1,6 +1,6 @@
-import {StackScreenProps} from '@react-navigation/stack';
 import React, {useContext} from 'react';
 import {Button, FlatList, Text, View} from 'react-native';
+import {StackScreenProps} from '@react-navigation/stack';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {
   AuthContext,
@@ -8,24 +8,29 @@ import {
   ProductsContext,
   ProductsContextProps,
 } from '../..';
+import {ProductsStackParams} from '../../navigator/ProductsNav';
 import {styles} from './style';
 
-interface NavigationProps extends StackScreenProps<any, any> {}
+interface NavigationProps
+  extends StackScreenProps<ProductsStackParams, 'ProductsScreen'> {}
 
 export const ProductsScreen = ({navigation}: NavigationProps) => {
   const {user, logout} = useContext<AuthContextProps>(AuthContext);
   const {products} = useContext<ProductsContextProps>(ProductsContext);
   const {navigate} = navigation;
+
+  //todo Pull to refresh
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Products</Text>
       <Text style={styles.title}>User: {user?.nombre}</Text>
       <FlatList
         data={products}
-        renderItem={({item: {nombre}}) => (
+        renderItem={({item: {nombre, _id}}) => (
           <TouchableOpacity
             activeOpacity={0.8}
-            onPress={() => navigate('ProductScreen')}>
+            onPress={() => navigate('ProductScreen', {id: _id, name: nombre})}>
             <Text style={styles.productName}>{nombre}</Text>
           </TouchableOpacity>
         )}
