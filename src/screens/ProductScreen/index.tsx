aa28712f5add0fd04edf078eefa34752d2332c54
RunyShark <spartadragon117@live.com.mx>
2022-10-23 19:30:50 -0500

    renderProduct

--- src/screens/ProductScreen/index.tsx
+++ src/screens/ProductScreen/index.tsx
@@ -1,15 +1,21 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React, {useContext} from 'react';
 import {Button, Text, View} from 'react-native';
 import {AuthContext, AuthContextProps} from '../..';
+import {ProductsStackParams} from '../../navigator/ProductsNav';
 import {styles} from './style';
+interface NavigationProps
+  extends StackScreenProps<ProductsStackParams, 'ProductScreen'> {}
 
-export const ProductScreen = () => {
-  const {user, logout} = useContext<AuthContextProps>(AuthContext);
+export const ProductScreen = ({route}: NavigationProps) => {
+  const {logout} = useContext<AuthContextProps>(AuthContext);
+  const {params} = route;
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Protected Screen</Text>
-      <Text style={styles.title}>Name: {user?.nombre}</Text>
+      <Text style={styles.title}>Id: {params?.id}</Text>
+      <Text style={styles.title}>Name: {params?.name}</Text>
       <Button title="logout" color="#5856D6" onPress={logout} />
     </View>
   );
