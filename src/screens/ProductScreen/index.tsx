b97df932e714732994f1257e089b1221d240461d
RunyShark <spartadragon117@live.com.mx>
2022-10-23 19:54:14 -0500

    selector

--- src/screens/ProductScreen/index.tsx
+++ src/screens/ProductScreen/index.tsx
@@ -1,38 +1,39 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react-hooks/exhaustive-deps */
 import {StackScreenProps} from '@react-navigation/stack';
-import React, {useContext, useEffect} from 'react';
-import {Button, Text, TouchableOpacity, View} from 'react-native';
-import {AuthContext, AuthContextProps} from '../..';
+import React, {useEffect} from 'react';
+import {Button, Text, View} from 'react-native';
+import {ScrollView, TextInput} from 'react-native-gesture-handler';
 import {ProductsStackParams} from '../../navigator/ProductsNav';
 import {styles} from './style';
 interface NavigationProps
   extends StackScreenProps<ProductsStackParams, 'ProductScreen'> {}
 
 export const ProductScreen = ({route, navigation}: NavigationProps) => {
-  const {logout} = useContext<AuthContextProps>(AuthContext);
   const {params} = route;
-  const {setOptions, navigate} = navigation;
+  const {setOptions} = navigation;
 
   useEffect(() => {
     setOptions({
       title: params?.name ? params?.name : 'New product',
-      headerRight: () => (
-        <TouchableOpacity
-          activeOpacity={0.8}
-          style={styles.options}
-          onPress={() => navigate('ProductScreen', {name: 'New Product'})}>
-          <Text>Add </Text>
-        </TouchableOpacity>
-      ),
     });
   }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Protected Screen</Text>
-      <Text style={styles.title}>Id: {params?.id}</Text>
-      <Text style={styles.title}>Name: {params?.name}</Text>
-      <Button title="logout" color="#5856D6" onPress={logout} />
+      <ScrollView>
+        <Text style={styles.label}>Name product: {params?.name}</Text>
+        <TextInput placeholder="Product" style={styles.textInput} />
+
+        <Text style={styles.label}>Select category:</Text>
+        <Button title="save" onPress={() => {}} color="#5856D6" />
+        <View style={styles.subView}>
+          <Button title="Camera" onPress={() => {}} color="#5856D6" />
+          <View style={{width: 10}} />
+          <Button title="Gallery" onPress={() => {}} color="#5856D6" />
+        </View>
+      </ScrollView>
     </View>
   );
 };
--- src/screens/ProductScreen/style.ts
+++ src/screens/ProductScreen/style.ts
@@ -3,8 +3,8 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 10,
+    marginHorizontal: 10,
   },
   title: {
     fontSize: 20,
@@ -12,5 +12,22 @@ export const styles = StyleSheet.create({
   },
   options: {
     marginRight: 20,
+  },
+  label: {
+    fontSize: 18,
+  },
+  textInput: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,
+    borderColor: 'rgb(0,0,0,0.2)',
+    height: 45,
+    marginBottom: 10,
+  },
+  subView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
   },
 });
